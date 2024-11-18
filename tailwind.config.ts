36ce1769ad67ca1fa57bff0ab121448724f80222
Shriyansh7870 <shriyansh7870@gmail.com>
2024-11-19 01:57:40 +0530

    again

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from "tailwindcss";
-
+// tailwind.config.ts
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // Adding a fallback for any other files
   ],
   theme: {
     extend: {
